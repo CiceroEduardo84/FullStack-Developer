2f0ff0f4fd0752ca6e14ac2ae038366d4f03f9bb
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2025-01-08 23:31:35 -0300

    add introduction js

---
+++ Javascript/introdução/02-comentarios/scripts.js
@@ -0,0 +1,15 @@
+// console.log() imprimi no console um testo.
+console.log("Hello World!"); //Pode-se colocar do lado
+
+/*
+  Comentários de multiplas linhas
+  Comentários são trechos que podem ser adicionados junto ao código.
+
+  O navegador irá ignorar.
+
+  Porque ultilizar comentários durante seus estudos?
+    - Fornecer informações adicionais.
+    - Deixar lembretes de coisas a fazer.
+    - Relembrar no futuro o que aquilo faz.
+    - Etc...
+    */
---
+++ Javascript/scripts.js
@@ -0,0 +1,9 @@
+//Sintaxe correta.
+console.log("Olá, tudo bem?");
+
+//Sintaxe incorretas.
+// con.sole.log("Olá, tudo bem?");
+
+//No js ";" é opcinal.
+console.log("test 1");
+console.log("test 2")
