1aaaa6def2a0933a88a16b6a3cd22cb9704d88cc
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2025-10-23 12:57:19 -0300

    add: create middleware authorization in the route product

--- JWT/Project_base/src/routes/products.routes.ts
+++ JWT/Project_base/src/routes/products.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express"
 import { ProductsController } from "@/controllers/products-controller"
 import { ensureAuthenticadted } from "@/middlewares/ensureAuthenticated"
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization"
 
 const productsRoutes = Router()
 const productsController = new ProductsController()
 
+// Autenticação e autorização em todas as rotas seguir
+// productsRoutes.use(verifyUserAuthorization(["sale", "admin"]))
+
 productsRoutes.get("/", productsController.index)
-productsRoutes.post("/",ensureAuthenticadted, productsController.create)
+productsRoutes.post("/",ensureAuthenticadted, verifyUserAuthorization(["sale", "admin"]), productsController.create)
 
 export { productsRoutes }
