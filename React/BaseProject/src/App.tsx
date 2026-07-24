95f2b45070c620d701abe1d84842d61cd6e18a21
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2026-07-23 23:52:25 -0300

    How a hooks works

--- React/BaseProject/src/App.tsx
+++ React/BaseProject/src/App.tsx
@@ -1,11 +1,17 @@
 import "./global.css";
 import { Button } from "./components/button";
 import styles from "./app.module.css";
+import { useMessage } from "./hooks/useMessage";
 
 export function App() {
+  const { show } = useMessage({ name: "teste", age: 18 });
+
   return (
     <div className={styles.container}>
-      <Button name="Adicionar" onClick={() => alert("Adicionar")} />
+      <Button
+        name="Adicionar"
+        onClick={() => show("Mensagem do meu proprio hook")}
+      />
       <span>0</span>
       <Button name="Remover" onClick={() => alert("Remover")} />
     </div>
---
+++ React/Hooks.md
@@ -0,0 +1,12 @@
+# [[React]] - Hooks
+
+Permite que você use estados e outros recursos do React sem escrever um classe. Encapsula funcionalidades e facilita o reaproveitamento da sua lógica.
+
+## Exemplos de Hooks
+ - useState: Permite adicionar um variável de estado no componente.
+ - useEffect: Utilizando no ciclo de vida do componente e permite trabalhar com side-effects(efeitos colaterais).
+## Nome do hook
+
+ ==use==NomeDoHook
+ 
+ 
