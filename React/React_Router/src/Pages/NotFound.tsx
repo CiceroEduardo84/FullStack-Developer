a79a9e4bff588dd7db4e8c1d5885debb66ed5e94
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2026-08-10 23:12:24 -0300

    feat: add page  404

---
+++ React/React_Router/src/Pages/NotFound.tsx
@@ -0,0 +1,6 @@
+export function NotFound() {
+  return <div>
+    <h1>404 page not found</h1>
+    <a href="/">Voltar</a>
+  </div>
+}
--- React/React_Router/src/Routes/AppRoutes.tsx
+++ React/React_Router/src/Routes/AppRoutes.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router";
-import { Home } from "../Pages/home";
+import { Home } from "../Pages/Home";
 import { Products } from "../Pages/Products";
+import { NotFound } from "../Pages/NotFound";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" index element={<Home />} />
-      <Route path="/product" index element={<Products />} />
+      <Route path="/product?category=tvs" index element={<Products />} />
+
+      <Route path="*" index element={<NotFound />} />
     </Routes>
   );
 }
