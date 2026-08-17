ef6ac6222d4dd8ee89ea56f9262bf0464fad95f6
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2026-08-16 23:25:45 -0300

    feat: form in React

---
+++ React/Forms/src/App.tsx
@@ -0,0 +1,71 @@
+import { Controller, useForm } from "react-hook-form";
+import "./App.css";
+
+type FormData = {
+  name: string;
+  date: string;
+  subject: string;
+  description: string;
+};
+export default function App() {
+  const { control, handleSubmit } = useForm<FormData>({
+    defaultValues: { name: "", date: "", subject: "", description: "" },
+  });
+
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
+
+  return (
+    <div>
+      <h1>Evento</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="name"
+          render={({ field }) => (
+            <input type="text" placeholder="Nome do evento" {...field} />
+          )}
+        />
+
+        <span className="error">Nome é obrigatório</span>
+
+        <Controller
+          control={control}
+          name="date"
+          render={({ field }) => (
+            <input type="date" placeholder="Nome do evento" {...field} />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="subject"
+          render={({ field }) => (
+            <select {...field}>
+              <option value="" disabled>
+                Selecione...
+              </option>
+
+              <option value="technology">React</option>
+              <option value="entertainment">Node.js</option>
+              <option value="business">Javascript</option>
+              <option value="business">Typescript</option>
+            </select>
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="description"
+          render={({ field }) => (
+            <textarea placeholder="Descrição" rows={4} {...field} />
+          )}
+        />
+
+        <button type="submit">Salvar</button>
+      </form>
+    </div>
+  );
+}
